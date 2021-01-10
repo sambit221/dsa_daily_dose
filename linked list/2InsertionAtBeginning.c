05247cf3cea6e507d0b81af22b8d60dc2a67e2a3
sambit221 <56729866+sambit221@users.noreply.github.com>
2021-01-10 18:33:58 +0530

    adding insertion problems

--- linked list/2InsertionAtBeginning.c
+++ linked list/2InsertionAtBeginning.c
@@ -1,4 +1,4 @@
-// Wap to traverse, assign data and print data of a linked list,
+// Wap to insert a node at the beginning of a linked list,
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -8,7 +8,7 @@ struct Node{
     struct Node *next;
 };
 
-// traverse the linked list
+// traverse the linked list and printing each data
 void traversal(struct Node *ptr ){
     while (ptr != NULL){
         printf("Element = %d\n", ptr->data);
@@ -21,12 +21,14 @@ int main(int argc, char const *argv[]){
     struct Node * second;
     struct Node * third;
     struct Node * forth;
+    struct Node * ptr;
 
     // allocating memory for nodes in heap 
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
     forth = (struct Node *)malloc(sizeof(struct Node));
+    ptr = (struct Node *)malloc(sizeof(struct Node));
 
     // link 1st and 2nd node
     head -> data = 7;
@@ -45,7 +47,29 @@ int main(int argc, char const *argv[]){
     forth -> next = NULL; 
 
     // calling traversal function
+    printf("The Linked list before insertion :\n");
     traversal(head);
 
+    // inserting node at the beginning
+    ptr -> data = 5;
+    ptr -> next = head;
+
+    printf("After inserting node (data element = 5) at the beginning the linked list willl be : \n");
+    traversal(ptr);
     return 0;
 }
+
+/* ---------------output---------------------
+
+The Linked list before insertion :
+Element = 7
+Element = 8
+Element = 11
+Element = 66
+After inserting node (data element = 5) at the beginning the linked list willl be :
+Element = 5
+Element = 7
+Element = 8
+Element = 11
+Element = 66
+*/
