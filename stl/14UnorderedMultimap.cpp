1a640f36fe3e6b739b606891c175f0e38507524b
sambit221 <56729866+sambit221@users.noreply.github.com>
2021-03-06 02:16:29 +0530

    unordered multimaps added

--- stl/14UnorderedMultimap.cpp
+++ stl/14UnorderedMultimap.cpp
@@ -1,6 +1,6 @@
-// WAP to show operations using unordered_map (associative container)
+// WAP to show operations using unordered_multimap (associative container)
 
-// it is same as map but only diff is it will be under diff preprocessor, will not be sorted, subscript operators are allowed here
+// it is same as multimap but only diff is it will be under diff preprocessor, will not be sorted, subscript operators are not allowed here
 #include<iostream>
 #include<map> // to use map, multimap we need to include this headerfile
 #include<unordered_map> // to use unordered map
@@ -9,16 +9,17 @@ int main(int argc, char const *argv[]){
     pair<int, int> p = make_pair(10,20);
     cout<<p.first<<"-"<<p.second<<endl;
 
-    unordered_map<int, int> mp;
-    mp[4] = 400;
-    mp[3] = 300;
+    unordered_multimap<int, int> mp;
+    // As subscript operator is not allowed in multimap, unordered_multimap
+    //mp[4] = 400;
+    //mp[3] = 300;
     mp.insert(make_pair(3, 100));
     mp.insert(make_pair(10, 2100));
-    mp[10] = 700; // will update the value
+    //mp[10] = 700; // will update the value
     mp.insert(make_pair(20, 900)); 
     mp.insert(make_pair(20, 920)); // will not be updated as same as map
-    mp[20] = 700; //updated the value
-
+    //mp[20] = 700; //updated the value
+    
     // Iterate
     for (auto it = mp.begin(); it != mp.end(); it++){ // iterator of map returns a pair
         cout<<it->first<<"----------"<<it->second<<endl; // as iterators are more similar to pointers so we use -> instead of . operator
@@ -28,8 +29,8 @@ int main(int argc, char const *argv[]){
 }
 /*-----------output---------
 10-20
-20----------700
-4----------400
-10----------700
-3----------300
+20----------920
+20----------900
+10----------2100
+3----------100
 */
