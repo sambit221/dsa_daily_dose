26a9d54687af1d5d9816fd103755c97a90641ca8
sambit221 <56729866+sambit221@users.noreply.github.com>
2021-04-16 19:20:49 +0530

    files added

---
+++ arrays/Qs35FindTheMissingElement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[]){
+    int arr[]={6,7,8,9,10,11,12,14,15,16,17};
+    int l=arr[0],n=11, h=n-1;
+    int diff = l;
+    for (int i = 0; i < h; i++){
+        if (arr[i]-i != diff){
+           printf("Missing element is %d",i+diff); 
+           break;
+        }   
+    }  
+    return 0;
+}
---
+++ arrays/Qs36FindMultipleMisiingElements.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[]){
+    int arr[]={6,7,8,9,10,12,13,14,16,17,18,19,21,22,23};
+    int l=arr[0],h=14;
+    int diff = l;
+    for (int i = 0; i < h; i++){
+        if (arr[i]-i != diff){
+           printf("Missing element is %d\n",i+diff); 
+           diff = arr[i]-i; 
+           continue;
+        }   
+    }   
+    return 0;
+}
